c1f8712ac0c0b02bcc8870d54b163cbcfcb45a93
AswiniVerma <72538717+AswiniVerma@users.noreply.github.com>
2025-05-08 01:25:06 +0530

    News push

---
+++ editorDashboard/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewsForm from './newsEditor/NewsForm';
+
+function App() {
+  return (
+    <div className="App">
+      <NewsForm />
+    </div>
+  );
+}
+
+export default App;
