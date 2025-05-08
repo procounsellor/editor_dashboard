f841dcc974d9ff08c39985e5ce878fae97fda846
AswiniVerma <72538717+AswiniVerma@users.noreply.github.com>
2025-05-08 23:21:40 +0530

    Sidebars and navbar

--- editorDashboard/src/App.tsx
+++ editorDashboard/src/App.tsx
@@ -2,14 +2,45 @@ import React from 'react';
 import NewsForm from './newsEditor/NewsForm';
 import NewsList from './newsEditor/NewsList';
 import './App.css';
+import Navbar from './navbar/Navbar';
 
 const App: React.FC = () => {
+  const sidebarItems = [
+    "News",
+    "Events",
+    "Deadlines",
+    "Colleges",
+    "Courses",
+    "Exams",
+    "Form Filling",
+    "Stress Management",
+    "Time Management",
+    "Mental Wellness",
+    "Personal Growth"
+  ];
+
   return (
+
+    <div className="app-wrapper">
+      <Navbar />
     <div className="app-container">
       <div className="app-content">
         <NewsList />
-        <NewsForm />
+
+        <div className="form-and-sidebar">
+          <NewsForm />
+          
+          <div className="sidebar-wrapper">
+  <div className="sidebar-cards">
+    {sidebarItems.map(item => (
+      <div key={item} className="sidebar-card">{item}</div>
+    ))}
+  </div>
+</div>
+
+        </div>
       </div>
+    </div>
     </div>
   );
 };
